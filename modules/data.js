bda3dbc1a9ff90c01296aeb70167b0d98508d84c
nicolas <nmalonsojuan@gmail.com>
2022-03-11 19:28:09 +0200

    added the data

---
+++ modules/data.js
@@ -0,0 +1,57 @@
+const data = {
+    'rock_songs': [
+        'Queen - Bohemian Rhapsody',
+        'Steppenwolf - Born to Be Wild',
+        'Pink Floyd - Another Brick in the Wall',
+        'Lynyrd Skynyrd - Free bird',
+        'Led Zeppelin - Stairway to Heaven',
+        'Eric Clapton - Layla',
+        'Bob Dylan - The Times They Are Changin',
+        'Joan Jett - I Love Rock n Roll',
+        'Beatles - Yesterday',
+        'Van Halen - Cant Stop Lovin You',
+        'Aerosmith - Dream On',
+        'Scorpions - No One Like You',
+        'Red Hot Chilli Peppers - Under the Bridge',
+        'Nirvana - Come as You Are',
+        'ZZ Top - La Grange',
+        'Foo Fighters - Best of You',
+        'Pearl Jam - Even Flow',
+        'Jimmy Hendrix - Voodoo Child',
+        'Chuck Berry - Johnny B Goode'
+    ],
+    'metal_songs': [
+        'Iron Maiden - The Trooper',
+        'Ozzy Osbourne - Crazy Train',
+        'Judas Priest - Breaking the Law',
+        'Metallica - Enter Sandman',
+        'Pantera - Walk',
+        'Dio - Holy Diver',
+        'System of a Down - Lonely Day',
+        'Black Sabbath - Iron Man',
+        'Helloween - I Want Out',
+        'Megadeth - Symphony of Destruction',
+        'Disturbed - Down with the Sickness',
+        'Motorhead - Ace of Spades',
+        'Five Finger Death Punch - Wrong Side of Heaven',
+        'Dragon Force - Through the Fire and Flames',
+        'Tool - Schism',
+        'Rammstein - Mein Herz Brennt',
+        'Avenged Sevenfold - Hail to the King',
+        'WASP - Wild Child'
+    ],
+    "quotes": [
+        'When all is one and one is all, thats what it is to be a rock and not to roll. -Led Zeppelin',
+        'Im into rock n roll because rock n roll, to me, means freedom. -Ozzy Osbourne',
+        'If you tried to give rock and roll another name, you might call it Chuck Berry. -John Lennon',
+        'Rock n roll is an attitude, its not a musical form of a strict sort. -Lester Bangs',
+        'Rock and roll keeps you in a constant state of juvenile delinquency. -Eddie Spaghetti',
+        'Rock and roll doesnt necessarily mean a band. Its that question of trying to be immortal. -Malcolm Mclaren',
+        'No change in musical style will survive unless it is accompanied by a change in clothing style. Rock is to dress up to. -Frank Zappa',
+        'Music is my religion. -Jimmy Hendrix',
+        'Dont criticize what you cant understand. -Bob Dylan',
+        'I wont be a rock star. I will be a legend. -Freddie Mercury',
+        'If its illegal to rock n roll, throw my ass in jail! -Kurt Cobain'
+    ]
+}
+
